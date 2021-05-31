7ed5120265daafd915dd2c4d3607c3bf84a20149
Satej Bidvai <satej.bidvai@gmail.com>
2021-06-01 02:33:42 +0530

    Refactor content script

--- content.js
+++ content.js
@@ -1,100 +1,58 @@
-//flux
+/**
+ * Helper Strings
+ */
+const FRAME = 'ptifrmtgtframe';
+const SEARCH_BTN = 'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH';
+const SAVE_BTN = 'ICSave';
+const NEW_SEARCH_BTN = 'CLASS_SRCH_WRK2_SSR_PB_NEW_SEARCH';
+const ITEM_CLASS = 'SSSHYPERLINKBOLD';
+const ERP_BS = '&nbsp;&nbsp;&nbsp;&nbsp; ';
 
-// For "SEARCH" && "OK" button
+/**
+ * Helper Functions
+ */
+const helperById = (id) => {
+	return document
+		.getElementById(FRAME)
+		.contentWindow.document.getElementById(id);
+};
+
+const helperByClassName = (className) => {
+	return document
+		.getElementById(FRAME)
+		.contentWindow.document.getElementsByClassName(className);
+};
+
+/**
+ * Code
+ */
+let courses = [];
+chrome.storage.sync.get('selectedCourses', (e) => {
+	e.selectedCourses.forEach((course) => {
+		const temp = course.split(' : ')[0].split(' ');
+		courses.push(temp[0] + ERP_BS + temp[1]);
+	});
+});
 
 setInterval(function () {
-	if (
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById(
-				'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH'
-			) != null
-	)
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById(
-				'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH'
-			)
-			.click();
-
-	if (
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById('#ICSave') != null
-	)
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById('#ICSave')
-			.click();
+	helperById(SEARCH_BTN) && helperById(SEARCH_BTN).click();
+	helperById(SAVE_BTN) && helperById(SAVE_BTN).click();
 }, 2500);
 
-// For results in console + audio
-
 setInterval(function () {
-	if (
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById(
-				'CLASS_SRCH_WRK2_SSR_PB_NEW_SEARCH'
-			) != null
-	) {
-		// fetches length of total subjects with seats available
-		t = document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementsByClassName(
-				'SSSHYPERLINKBOLD'
-			).length;
-		// prints in console
+	if (helperById(NEW_SEARCH_BTN) != null) {
+		t = helperByClassName(ITEM_CLASS).length;
 		for (var i = 0; i < t; i++) {
-			// HSS and GS courses are being looked for - loop is maximum possible
 			if (
-				document
-					.getElementById('ptifrmtgtframe')
-					.contentWindow.document.getElementsByClassName(
-						'SSSHYPERLINKBOLD'
-					)
-					[i].innerHTML.search('HSS') != -1 ||
-				document
-					.getElementById('ptifrmtgtframe')
-					.contentWindow.document.getElementsByClassName(
-						'SSSHYPERLINKBOLD'
-					)
-					[i].innerHTML.search('GS') != -1
+				helperByClassName(ITEM_CLASS)[i].innerHTML.search(courses[0]) !=
+				-1
 			) {
-				// prints log on console
-				console.log(
-					document
-						.getElementById('ptifrmtgtframe')
-						.contentWindow.document.getElementsByClassName(
-							'SSSHYPERLINKBOLD'
-						)[i].innerHTML
-				);
-				// sound
-				var audio = new Audio(
-					'http://soundbible.com/grab.php?id=1789&type=wav'
-				);
-				audio.play();
+				console.log('Found');
 			}
 		}
 	}
 }, 5000);
 
-// For "START A NEW SEARCH" button
-
 setInterval(function () {
-	if (
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById(
-				'CLASS_SRCH_WRK2_SSR_PB_NEW_SEARCH'
-			) != null
-	)
-		document
-			.getElementById('ptifrmtgtframe')
-			.contentWindow.document.getElementById(
-				'CLASS_SRCH_WRK2_SSR_PB_NEW_SEARCH'
-			)
-			.click();
+	if (helperById(NEW_SEARCH_BTN) != null) helperById(NEW_SEARCH_BTN).click();
 }, 10000);
-
-//flux
